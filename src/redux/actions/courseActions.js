267af370ece46a14689b8865b078c9e9c078a577
renelopez <rene.lopez.cano@gmail.com>
2019-03-25 00:33:30 -0600

    Adding all

---
+++ src/redux/actions/courseActions.js
@@ -0,0 +1,47 @@
+import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    dispatch(beginApiCall());
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch) {
+    dispatch(beginApiCall());
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
