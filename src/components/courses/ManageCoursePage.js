0cfc9d6fff31c39d8107a4292369416dd4ea36bc
rene.lopez <rene.lopez.cano@gmail.com>
2019-03-25 18:53:53 -0600

    Adding testing

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -45,8 +45,21 @@ function ManageCoursePage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+
+    setErrors(errors);
+    return Object.keys(errors.length) === 0;
+  }
+
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid) return;
     setSaving(true);
     saveCourse(course)
       .then(() => {
