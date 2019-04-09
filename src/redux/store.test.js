b7452ff2a46fb67421cc24df12b6cb50b77f36f2
rene.lopez <rene.lopez.cano@gmail.com>
2019-04-09 15:03:13 -0500

    Add create course test

--- src/redux/store.test.js
+++ src/redux/store.test.js
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 import rootReducer from "./reducers/index";
 import initialState from "./reducers/initialState";
 import * as authorActions from "./actions/authorActions";
+import * as courseActions from "./actions/courseActions";
 
 xit("should handle creating courses without author", () => {
   //arrange
@@ -18,4 +19,24 @@ xit("should handle creating courses without author", () => {
   //assert
   const createdAuthor = store.getState().authors[0];
   expect(createdAuthor).toEqual(createdAuthor);
+});
+
+it("should handle creating courses", () => {
+  //arrange
+  const store = createStore(rootReducer, initialState);
+  const course = {
+    title: "Securing Vue Apps",
+    slug: "vue-authentication-security",
+    authorId: 1,
+    category: "JavaScript"
+  };
+
+  //act
+
+  const createAuthorAction = courseActions.createCourseSuccess(course);
+  store.dispatch(createAuthorAction);
+
+  //assert
+  const createdCourse = store.getState().courses[0];
+  expect(createdCourse).toEqual({ title: "Security 101" });
 });
